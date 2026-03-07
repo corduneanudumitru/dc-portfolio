ff57c65d9b01353b8327fa8fc90bbd724b8ec08a
Dumitru Corduneanu <dumitrucorduneanu@Dumitrus-MacBook-Pro.local>
2026-03-07 13:34:14 +0000

    Connect schemas to Sanity Studio

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,5 +1,11 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import blockContent from '../../../sanity/schemas/blockContent'
+import project from '../../../sanity/schemas/project'
+import post from '../../../sanity/schemas/post'
+import navigation from '../../../sanity/schemas/navigation'
+import siteSettings from '../../../sanity/schemas/siteSettings'
+import about from '../../../sanity/schemas/about'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [blockContent, project, post, navigation, siteSettings, about],
 }
